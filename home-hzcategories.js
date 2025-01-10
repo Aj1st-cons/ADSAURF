939c9140f3c3f4a41705643025d3e985a24dd1d7
Aj1st-cons <admin@aj1st.com>
2025-01-10 13:39:46 +0400

    Update home-hzcategories.js

--- home-hzcategories.js
+++ home-hzcategories.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("home-hz-items");
-  fetch("https://adsaurf.pages.dev/home-hzcategoryitems.html")
+  fetch("https://adsaurf.pages.dev/home-hz-items.html")
     .then(response => response.text())
     .then(html => {
       container.innerHTML = html;
