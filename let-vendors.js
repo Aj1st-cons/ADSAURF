7ed7b4d551ef6309021d546c5198055f101883d3
Aj1st-cons <admin@aj1st.com>
2025-08-17 21:36:08 +0400

    Update let-vendors.js

--- let-vendors.js
+++ let-vendors.js
@@ -1,8 +1,32 @@
-export let vendors = {
-  "DawarAlDhaid": { lat: 25.300490, lng: 55.376445, categories: ["Cosmetics"] },
-  "BestCafe": { lat: 25.300452, lng: 55.376473, categories: ["Cafeteria"] },
-  "Occations": { lat: 25.3006599, lng: 55.3750735, categories: ["Cakes"] },
-  "Rapid": { lat: 25.3018266, lng: 55.378627, categories: ["Salon"] },  
-  "AlRas": { lat: 25.3006599, lng: 55.3750735, categories: ["Salon"] },
-  "nbys": { lat: 25.301058, lng: 55.3759718, categories: [""] }
+let vendors = {
+  "DawarAlDhaid": { 
+    lat: 25.300490, 
+    lng: 55.376445, 
+    categories: ["Cosmetics"], 
+    image: "https://example.com/images/dawar.jpg",
+    uploaderId: "AAA888"
+  },  
+  "BestCafe": { 
+    lat: 25.300452, 
+    lng: 55.376473, 
+    categories: ["Cafeteria"], 
+    image: "https://example.com/images/bestcafe.jpg",
+    uploaderId: "AAA888"
+  },  
+  "Occations": { 
+    lat: 25.3006599, 
+    lng: 55.3750735, 
+    categories: ["Cakes"], 
+    image: "https://example.com/images/occations.jpg",
+    uploaderId: "AAA888"
+  },  
+  "Rapid": { 
+    lat: 25.3018266, 
+    lng: 55.378627, 
+    categories: ["Salon"], 
+    image: "https://example.com/images/rapid.jpg",
+    uploaderId: "AAA888"
+  }
 };
+
+export default vendors;
